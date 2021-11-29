afb23f235463bb27945570e449ce32f8be854274
anthonyR05 <anthrosa12@gmail.com>
2021-11-29 13:01:53 -0600

    Initial Commit

---
+++ learngithub/learngithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learngithubApp.swift
+//  learngithub
+//
+//  Created by PM CSS3 - Anthony Rosales on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct learngithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
